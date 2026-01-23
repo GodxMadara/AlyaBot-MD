e514187c55386b6e859c31c036ea2d6afdcaa304
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2026-01-23 13:31:21 -0400

    Add files via upload

--- commands/gachareserved.js
+++ commands/gachareserved.js
@@ -1,25 +1,26 @@
-import chalk from 'chalk';
+import chalk from 'chalk'
 
 const limpiarPersonajesReservados = () => {
   try {
-    // if (!global.db) global.db = {}
-   // if (!global.db.data) global.db.data = {}
-   // if (!global.db.data.chats) global.db.data.chats = {}
-
     const chats = global.db.data.chats
-
+    const now = Date.now()
     for (const chatId of Object.keys(chats)) {
       const chat = chats[chatId]
-
-      if (!Array.isArray(chat.personajesReservados)) {
+      if (!chat.personajesReservados || !Array.isArray(chat.personajesReservados)) {
         chat.personajesReservados = []
-      } else {
-        chat.personajesReservados.length = 0
+        continue
+      }      
+      const nuevosPersonajesReservados = chat.personajesReservados.filter(personaje => {
+        const expirado = personaje.expiresAt && now > personaje.expiresAt
+        const yaReclamado = chat.users && Object.values(chat.users).some(u => u.characters && u.characters.some(c => c.name === personaje.name))        
+        return !expirado && !yaReclamado
+      })      
+      if (chat.personajesReservados.length !== nuevosPersonajesReservados.length) {
+        chat.personajesReservados = nuevosPersonajesReservados
       }
     }
-
   } catch (e) {
-  //  console.log(chalk.gray('🍄  Chat No Definido')) 
+    console.log(chalk.gray('Error limpiando personajesReservados'))
   }
 }
 
